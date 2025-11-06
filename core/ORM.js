2a2e76f5881aff29fc03c42062985d4d085c7698
Eguerraz <milotriqchote27@gmail.com>
2025-11-06 14:17:12 +0100

    mise a joure du env

--- core/ORM.js
+++ core/ORM.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes, Op } = require('sequelize');
-const sequelize = new Sequelize('taches', 'symfony', 'Este2003.', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: '127.0.0.1',
   dialect: 'mysql',
 });
